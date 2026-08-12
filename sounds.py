4bcfd97746451971a340ce2ddeb94bb74d037cb0
karstenawney <karstenyawney@gmail.com>
2026-08-12 12:19:31 -0600

    Revise phoneme counts and output formatting
    Updated phoneme counts and vowel groupings. Changed output format for phonemes and consonants.

--- sounds.py
+++ sounds.py
@@ -2,40 +2,44 @@ print ("There are 26 letters  in english")
 print ("They are split into vowels and consonants")
 print ("Vowels: aeiou(y)")
 print ("Consonants: bcdfghjklmnpqrstvwx(y)z")
-print ("There are 39 phonemes in english")
+print ("There are 40 phonemes in english")
 print ("They are also split into vowels and consonants")
-print ("Vowels are split into 3 groups: Short, Long and Gliding")
+print ("Vowels are split into 4 groups: Short, Long, R-defined and Gliding")
 print ()
 print ("Short vowels:")
-print ("/a/ as in cat")
-print ("/e/ as in get")
-print ("/i/ as in sit")
-print ("/o/ as in hot")
-print ("/u/ as in cup")
+print ("(a) as in cat")
+print ("(e) as in get")
+print ("(i) as in sit")
+print ("(o) as in hot")
+print ("(u) as in cup")
 print ()
 print ("Long vowels:")
-print ("/a:/ as in face")
-print ("/e:/ as in see")
-print ("/i:/ as in price")
-print ("/o:/ as in boat")
-print ("/u:/ as in cute")
+print ("(a:) as in face")
+print ("(e:) as in see")
+print ("(i:) as in price")
+print ("(o:) as in boat")
+print ("(u:) as in cute")
 print ()
-print ("Gliding Vowels (R-defined):")
-print ("/ar/ as in car")
-print ("/ear/ as in near")
-print ("/ir/ as in bird")
-print ("/oy/ as in boy")
-print ("/oo/ as in food")
-print ("/ou/ as in mouth")
-print ("/ur/ as in pure")
+print ("R-defined:")
+print ("(a*) as in car")
+print ("(e*) as in err")
+print ("(i*) as in bird")
+print ("(o*) as in origin")
+print ("(u*) as in pure")
+print ()
+print ("Gliding Vowels:")
+print ("(ear) as in near")
+print ("(oy) as in boy")
+print ("(oo) as in food")
+print ("(ow) as in cow or mouth")
 print ()
 print ("Consonants are split into 5 groups: Plosives, Affricates, Fricates, Nasals and Approximates")
 print ()
-print ("Plosives: /p/, /b/, /t/, /d/, /k/, /g/")
-print ("Affricates: /ch/, /j/")
-print ("Fricatives: /f/, /v/, /th/, /s/, /z/, /sh/, /sz/, /h/")
-print ("Nasals: /m/, /n/, /ng/")
-print ("Approximants: /l/, /r/, /w/")
+print ("Plosives: (p), (b), (t), (d), (k), (g)")
+print ("Affricates: (ch), (j)")
+print ("Fricatives: (f), (v), (th), (s), (z), (sh), (h)")
+print ("Nasals: (m), (n), (ng)")
+print ("Approximants: (l), (r), (w)")
 print ()
 print ("This translator is meant to get rid of all silent letters, and to make english spell like it sounds")
 print ("It does this by translating the words into sounds, and then translating the sounds into words")
@@ -43,10 +47,13 @@ print ("It does this by translating the words into sounds, and then translating 
 vowels = "aeiou"
 consonants = "bcdfghjklmnpqrstvwxyz"
 
+print (len("pbtdkgjfvszhmnlrwz"))
+
 text = input("Enter text to translate: ")
 text = text.lower()
-words = text.split()
 text = text.replace("ui", "oo")
+text = text.replace("qu", "kw")
+words = text.split()
 sounds = []
 for word in words:
     sound = []
@@ -54,77 +61,77 @@ for word in words:
     while len(word) > 0:
         if word[0] in vowels:
             if word[0:3] == "ear":
-                sound.append("/ear/")
+                sound.append("(ear)")
                 previous_letter = word[2:3]
                 word = word[3:]
             elif word[1:2] == "r":
-                sound.append(f"/{word[0]}r/")
-                previous_letter = word[1:2]
-                word = word[2:]
+                sound.append(f"({word[0]}*)")
+                previous_letter = word[0]
+                word = word[1:]
             elif word[:2] == "oy":
-                sound.append("/oy/")
+                sound.append("(oy)")
                 previous_letter = word[1:2]
                 word = word[2:]
             elif word[:2] == "oo":
-                sound.append("/oo/")
+                sound.append("(oo)")
                 previous_letter = word[1:2]
                 word = word[2:]
-            elif word[:2] == "ou":
-                sound.append("/ou/")
+            elif word[:2] == "ou" or word[:2] == "ow":
+                sound.append("(ow)")
                 previous_letter = word[1:2]
                 word = word[2:]
             else:
                 if (((previous_letter in consonants and word[1:2] in consonants) or (word[1:2] in consonants and word[1:2] == word[2:3]) or (word[1:3] == "ck") or (word[1:4] == "tch")) and word[-1] != "e") or (word[-1] == "e" and len(word) == 1):
-                    sound.append(f"/{word[0]}/")
+                    sound.append(f"({word[0]})")
                     previous_letter = word[0]
                     word = word[1:]
                 elif word[0] in vowels and word[1:2] in vowels:
-                    sound.append(f"/{word[0]}:/")
-                    sound.append(f"/{word[1:2]}/")
+                    sound.append(f"({word[0]}:)")
+                    sound.append(f"({word[1:2]})")
                     previous_letter = word[1:2]
                     word = word[2:]
                 else:
-                    sound.append(f"/{word[0]}:/")
+                    sound.append(f"({word[0]}:)")
                     previous_letter = word[0]
                     word = word[1:]
         else:
             # consonants
             if word.startswith("sh"):
-                sound.append("/sh/")
+                sound.append("(sh)")
                 previous_letter = "h"
                 word = word[2:]
             elif word.startswith("th"):
-                sound.append("/th/")
+                sound.append("(th)")
                 previous_letter = "h"
                 word = word[2:]
             elif word.startswith("ng"):
-                sound.append("/ng/")
+                sound.append("(ng)")
                 previous_letter = "g"
                 word = word[2:]
             elif word.startswith("ch"):
-                sound.append("/ch/")
+                sound.append("(ch)")
                 previous_letter = "h"
                 word = word[2:]
             elif word[0] == "s":
-                if not sound or (sound and sound[-1] in ["/p/", "/t/", "/k/", "/f/", "/th/"]):
-                    sound.append("/s/")
+                if not sound or (sound and sound[-1] in ["(p)", "(t)", "(k)", "(f)", "(th)"]):
+                    sound.append("(s)")
                     previous_letter = "s"
                     word = word[1:]
                 else:
-                    sound.append("/sz/")
+                    sound.append("(z)")
                     previous_letter = "s"
                     word = word[1:]
             elif word[0] == "c":
                 if word[1:2] in "eiy":
-                    sound.append("/s/")
+                    sound.append("(s)")
                     previous_letter = "c"
                     word = word[1:]
                 else:
-                    sound.append("/k/")
+                    sound.append("(k)")
                     previous_letter = "c"
                     word = word[1:]
-            elif word[0] in "pbtdkgjfvszhmnlrw":
-                sound.append(f"/{word[0]}/")
+            elif word[0] in "pbtdkgjfvszhmnlrwz":
+                sound.append(f"({word[0]})")
                 previous_letter = word[0]
                 word = word[1:]
             else:
@@ -135,3 +142,10 @@ for word in words:
     sounds.append(sound)
 
 print (sounds)
+textsounds = ""
+for sound in sounds:
+    textsound = ""
+    for s in sound:
+        textsound += s
+    textsounds += textsound + " "
+print (textsounds)
